edf7d8e7766f9a0fbff4a9c9aafb40da7f9b9ead
Lush Alaz <lushalaz@gmail.com>
2026-03-30 21:48:09 +0000

    completely rewrote and finished new request parser

--- src/parse_request.c
+++ src/parse_request.c
@@ -1,44 +1,8 @@
-// experimental parsing w/ a lexer
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "request.h"
 #include "buffer.h"
-
-typedef enum {
-  // /r/n/r/n
-  NEWLINE,
-  // first line tokens
-  METHOD,
-  URI,
-  VERSION,
-  // header tokens
-  HEADER_NAME,
-  COLON,
-  HEADER_VAL,
-  // body
-  BODY,
-  // etc.
-  END,
-  ERROR,
-  EOF
-} TokenType;
-
-typedef enum {
-  // request line
-  RL_METHOD
-  RL_URI
-  RL_VERSION
-  // headers
-  HDR_HEADER_NAME,
-  HDR_HEADER_VAL,
-  // ...body
-  B_BODY
-} State;
-
-typedef struct Token {
-  TokenType type;
-  Buffer dat;
-} Token;
 
 typedef struct Lexer {
   char* dat;
@@ -49,99 +13,128 @@ typedef struct Lexer {
 
 // lexer function signatures
 char lexer_read_char(Lexer* lex);
-char lexer_peek_char(Lexer* lex);
 Buffer lexer_read_line(Lexer* lex);
+void lexer_next_line(Lexer* lex);
 Buffer lexer_read_to(Lexer* lex, char to);
 void lexer_skip_whitespace(Lexer* lex);
 Buffer lexer_read_to_whitespace(Lexer* lex);
 
-// fsm functions
-Token lex_req_line(Lexer* lex) {
-  switch
-}
-
-Token lex_headers(Lexer* lex) {
-  // todo
-}
-
-Token lex_body(Lexer* lex) {
-  
-}
-
-Token read_next_token(Lexer* lex, State st) {
-  switch (st) {
-    case RL_METHOD:
-    case RL_URI:
-    case RL_VERSION:
-      return lex_req_line(lex, st);
-    case HDR_HEADER_NAME:
-    case HDR_HEADER_VAL:
-      return lex_headers(lex, st);
-    case B_BODY:
-      return lex_body(lex, st);
-    default:
-      return {
-        .type: ERROR
-        .buf: NULL
-      };
-  }
-}
-
-Request parse_request(const Buffer raw_req) {
-  Lexer lex = {
-    .dat = raw_req.data;
-    .len = raw_req.len;
-    .pos = 0;
-    .chr = 0;
+Lexer lexer_from_buf(Buffer buf) {
+  return (Lexer) {
+    .dat = (char*) buf.data,
+    .len = buf.length,
+    .chr = (char) *buf.data
   };
+}
 
-  State curr_state = FIRST_LINE;
-  Token curr_token;
+int parse_request_line(Request* req, Lexer* lex) {
+  // first line
+  // rfc says to skip whitespace
+  lexer_skip_whitespace(lex);
 
-  while (State != END) {
-    Token tok = read_next_token(lex, curr_state);
+  Buffer method = lexer_read_to(lex, ' ');
+  Buffer uri = lexer_read_to(lex, ' ');
+  Buffer version = lexer_read_line(lex);
+
+
+  for (int i = 0; i < 4; i++)
+    if (strncmp((char*) method.data, method_strs[i], method.length) == 0) { req->method = (Method) i; break; }
+  if (req->method == (Method) -1) return -1;
+  
+  req->uri = buf_to_str(uri);
+  req->version = buf_to_str(version);
+
+  buf_free(&method);
+  buf_free(&uri);
+  buf_free(&version);
+
+  return 1;
+}
+
+int parse_request_headers(Request* req, Lexer* lex) {
+  Map headers;
+  map_init(&headers);
+  while (*(lex->dat + lex->pos) != '\r' && lex->pos < lex->len) {
+    Buffer header_name = lexer_read_to(lex, ':');
+    lexer_skip_whitespace(lex); // there will always be a space after the colon
+    Buffer header_value = lexer_read_line(lex);
+    if (map_set(&headers, (char*) header_name.data, (void*) buf_to_str(header_value)) == 0) {
+      buf_free(&header_name);
+      buf_free(&header_value);
+      return -1;
+    }
+
+    buf_free(&header_name);
+    buf_free(&header_value);
   }
   
+  req->headers = headers;
+  return 1;
+}
+
+Request req_parse_request(const Buffer raw_req) {
+  Request req = { .method = -1 };
+
+  Lexer lex = lexer_from_buf(raw_req);
+
+  if (parse_request_line(&req, &lex) == -1) 
+    return (Request) { .method = ERROR };
+
+  if (parse_request_headers(&req, &lex) == -1)
+    return (Request) { .method = ERROR };
+
+  lexer_next_line(&lex);
+  Buffer body = buf_new();
+  if (lex.len > lex.pos)
+    buf_append(&body, lex.dat + lex.pos, lex.len - lex.pos); // just append the rest of the data
+
+  req.body = body;
+  return req;
 }
 
 // lexer functions
 
 char lexer_read_char(Lexer* lex) {
-  if (lex->pos + 1 >= lex->len) return 0; // null indicates eof
+  if (lex->pos >= lex->len) return 0; // null indicates eof
   lex->chr = *(lex->dat + lex->pos++);
   return lex->chr; // so we dont have to lex->chr after every char read
-}
-
-char lexer_peek_char(Lexer* lex) {
-  return *(lex->dat + lex->pos);
 }
 
 Buffer lexer_read_line(Lexer* lex) {
   Buffer line = buf_new();
 
   while (lex->pos < lex->len) {
-    char c = lexer_read_char(lex);
-    if (c == "\r") { 
-      if (lexer_read_char(lex) == "\n") return line;
-    } else buf_append_one(&line, c);
+    lexer_read_char(lex);
+    if (lex->chr == '\r') { 
+      if (lexer_read_char(lex) == '\n')
+        return line;
+    } else buf_append_one(&line, lex->chr);
   }
+
+  return line;
+}
+
+void lexer_next_line(Lexer* lex) {
+  while (lex->pos < lex->len) {
+    if (lex->chr == '\r') { 
+      if (lexer_read_char(lex) == '\n')
+        return;
+    }
+    lexer_read_char(lex);
+  }
+
+  return;
 }
 
 Buffer lexer_read_to(Lexer* lex, char to) {
   Buffer content = buf_new();
-  while (lexer_read_char(lex) != to && lex->pos < lex->len) {
+  while (lex->pos < lex->len && lexer_read_char(lex) != to) {
     buf_append_one(&content, lex->chr);
   }
   return content;
 }
 
 void lexer_skip_whitespace(Lexer* lex) {
-  while (isspace(lex->chr) && lex->pos < lex->len) lexer_read_char(lex);
+  while (isspace(*(lex->dat + lex->pos)) && lex->pos < lex->len) lexer_read_char(lex);
   return;
-}
-
-Buffer lexer_read_to_whitespace(Lexer* lex) {
-  Buffer content = buf_new();
-  while (isspace(lexer_read_char(lex)) && lex->pos < lex->len) buf_append_one(&content, lex->chr);
-  return content;
 }
